147d2ad9414de0352413216601d3303c665fd677
Sabarico <55478627+Sabarico@users.noreply.github.com>
2026-03-05 14:10:58 +0530

    Add files via upload

--- style1.js
+++ style1.js
@@ -3,7 +3,8 @@ const ctx = canvas.getContext('2d');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 const letters = "01"; 
 const fontSize = 16;
 const columns = canvas.width / fontSize;
@@ -13,7 +14,7 @@ function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = "#008CFF"; // Bright Blue
+    ctx.fillStyle = "#008CFF"; 
     ctx.font = fontSize + "px monospace";
 
     for (let i = 0; i < drops.length; i++) {
@@ -24,3 +25,37 @@ function draw() {
     }
 }
 setInterval(draw, 33);
+const typingText=document.querySelector(".typing");
+const phrases=[
+    "a Software Developer",
+    "a Problem Solver",
+    "a Web Developer",
+    "a Computer Science Engineer",
+    "a Tech Enthusiast"
+];
+let phraseIndex=0;
+let charIndex=0;
+let isDeleting=false;
+let typeSpeed=100;
+function type(){
+const currentPhrase=phrases[phraseIndex];
+if(isDeleting){
+    typingText.textContent=currentPhrase.substring(0,charIndex-1);
+    charIndex--;
+    typeSpeed=50;
+}else{
+    typingText.textContent=currentPhrase.substring(0,charIndex+1);
+    charIndex++;
+    typeSpeed=150;
+}
+if(!isDeleting && charIndex ===currentPhrase.length){
+    isDeleting=true;
+    typeSpeed=2000;
+    }else if(isDeleting && charIndex ===0){
+        isDeleting=false;
+        phraseIndex=(phraseIndex+1)%phrases.length;
+        typeSpeed=500;
+    }
+    setTimeout(type,typeSpeed);
+}
+document.addEventListener("DOMContentLoaded",type);
